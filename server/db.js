e06d0b37ff5861826ab91cbbe7fd675bb5281ca9
luiscarlosfernandez77-crypto <claudiabarris.2.0@gmail.com>
2026-03-05 19:03:17 +0100

    jdjddjjt

--- server/db.js
+++
@@ -1,8 +0,0 @@
-const mysql = require('mysql2/promise');
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'super',
-    password: '1234',
-    database: 'sakila'
-});
-module.exports = pool;
